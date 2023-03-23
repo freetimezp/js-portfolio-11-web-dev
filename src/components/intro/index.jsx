338518535d4c3fa887aaf744421071d0d0efe732
freetimezp <kraftukrnet@ukr.net>
2023-03-23 15:54:01 +0200

    03. js-portfolio-11-web-dev. Intro component bg.

--- src/components/intro/index.jsx
+++ src/components/intro/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import cloud from '../../images/cloud.png';
+import cloudSoft from '../../images/cloud-soft.png';
+
+import './style.scss';
+
 const Intro = () => {
   return (
-    <div>
-      <p>Intro</p>
+    <div className='intro-section'>
+      <div className="vector-bg" id="parallax"></div>
+
+      <img src={cloud} alt="intro" className='cloud' />
+      <img src={cloudSoft} alt="intro" className='cloud-soft cloud' />
+
+      <div className="content">
+
+      </div>
     </div>
   );
 }
